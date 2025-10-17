fa00fcaaa732b6f53b90b10d182badeeeb99c791
Andrey  Marzan <3mrzana@mail.ru>
2025-10-18 01:03:03 +0300

    adding site files

---
+++ _next/static/chunks/fe746c19dc893354.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/3cbe42190722c878.js",
+  "static/chunks/turbopack-6dc88121814341a3.js"
+])
---
+++ _next/static/hplhVdvHOI4RojrvxhAeP/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/fe746c19dc893354.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
